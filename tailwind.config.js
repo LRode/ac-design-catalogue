09fc207af6f47378d54318925b1bee4f2881e12d
Lara Rode <lara.rode@ttt.studio>
2020-05-17 15:19:07 -0700

    Add home page and design routes, add get all designs api

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      body: ['Quicksand', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        pink: '#FFA7B2',
+        darkerPink: '#FF899B',
+        yellow: '#FFED58',
+        darkYellow: '#EBD895',
+        lightYellow: 'rgba(255, 237, 88, 0.23)',
+        darkBrown: '#4E4948',
+        grey: '#918D8D',
+        lightGrey: '#9C9C9C',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
